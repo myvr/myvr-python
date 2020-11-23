7bb8afb5642dd2c719e155ce154bcaa5da142577
alex.tarasiuk <alex.tarasuk@flyaps.com>
2020-11-23 15:13:54 +0200

    #13 - code updates

--- myvr/resources/photo.py
+++ myvr/resources/photo.py
@@ -2,6 +2,5 @@ from myvr.api.abstract import ListMixin, CreateMixin, UpdateMixin, DeleteMixin
 
 
 class Photo(CreateMixin, UpdateMixin, DeleteMixin, ListMixin):
-
     resource_url = '/photos/'
     model_name = 'Photo'
--- myvr/resources/property.py
+++ myvr/resources/property.py
@@ -2,9 +2,9 @@ from myvr.api.abstract import ListMixin, CreateMixin, UpdateMixin, DeleteMixin
 
 
 class Property(CreateMixin, UpdateMixin, DeleteMixin, ListMixin):
-
     resource_url = '/properties/'
     model_name = 'Property'
 
     def reset_rate(self, key: str, **kwargs):
-        return self.request('PUT', self.base_url + f'{key}/rates/', data=kwargs)
+        url = self.get_key_url(key) + 'rates/'
+        return self.request('PUT', url, data=kwargs)
