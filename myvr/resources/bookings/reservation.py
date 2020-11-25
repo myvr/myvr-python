412c6e3bce162c07c1e27fd658302da4d645ed2b
alex.tarasiuk <alex.tarasuk@flyaps.com>
2020-11-25 17:44:28 +0200

    #28 - Fix Reservation update method

--- myvr/resources/bookings/reservation.py
+++ myvr/resources/bookings/reservation.py
@@ -2,6 +2,7 @@ from functools import partialmethod
 
 from myvr.api.base import APIResource
 from myvr.api.mixins import CreateMixin, ListMixin, RetrieveMixin, UpdateMixin
+from myvr.api.myvr_objects import MyVRObject
 
 
 class Reservation(CreateMixin, RetrieveMixin, UpdateMixin, ListMixin):
@@ -14,3 +15,6 @@ class Reservation(CreateMixin, RetrieveMixin, UpdateMixin, ListMixin):
     decline = partialmethod(APIResource.object_action, path='decline/')
     approve = partialmethod(APIResource.object_action, path='approve/')
     assign = partialmethod(APIResource.object_action, path='assign/')
+
+    def update(self, key: str, **data) -> MyVRObject:
+        return self.request('PATCH', self.get_key_url(key), data=data)
