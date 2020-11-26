4dc1a589847846d40145d783cfae8ae8c65200eb
kirillchernovzazmic <74559985+kirillchernovzazmic@users.noreply.github.com>
2020-11-26 23:43:53 +0200

    Revert "Add crm objects"

--- myvr/resources/crm/contact_tag.py
+++
@@ -1,6 +0,0 @@
-from myvr.api.mixins import ModelViewSet
-
-
-class ContactTag(ModelViewSet):
-    resource_url = '/contact-tags/'
-    model_name = 'Contact Tag'
--- myvr/resources/crm/source.py
+++
@@ -1,6 +0,0 @@
-from myvr.api.mixins import CreateMixin, ListMixin, RetrieveMixin, UpdateMixin
-
-
-class Source(CreateMixin, RetrieveMixin, UpdateMixin, ListMixin):
-    resource_url = '/sources/'
-    model_name = 'Source'
